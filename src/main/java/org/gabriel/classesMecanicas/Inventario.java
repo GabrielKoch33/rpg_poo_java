ed1b2f0f405549ee727448d23c7ea8f8d756cd7b
Gabriel Koch da Silva <gabrielkochsilva@gmail.com>
2026-09-07 15:42:48 -0300

    criado os metodos da classe inventario, finalizar depois

--- src/main/java/org/gabriel/classesMecanicas/Inventario.java
+++ src/main/java/org/gabriel/classesMecanicas/Inventario.java
@@ -1,9 +1,33 @@
 package org.gabriel.classesMecanicas;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Inventario {
-    private int slots;
-    private Item[] inventory;
-    private int sumWeigth;
+    private final int tamanhoInventario = 30;
+    private final Set<Item> inventario;
+    private int pesoTotalInventario = 0;
+
+    public Inventario(){
+        this.inventario = new LinkedHashSet<Item>();
+    }
+
+    public boolean guardarItem(Item item){
+        return false;
+    }
+
+    public boolean descartarItem(Item item){
+        return false;
+    }
+
+    public boolean calculaPesoAtual(){
+        return false;
+    }
+
+    public boolean calculaEspacosLivres(){
+        return false;
+        // 30 - size
+    }
 
 }
 
